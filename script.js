7e34a95f548ec41aeb91671416f337b383fec7ff
Josh Gialis <jgialis@ucmerced.edu>
2021-08-24 14:48:58 -0700

    Final commit with refactored code. An addition was made where a player gets to select the range.

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 'use strict';
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max) + 1;
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
 }
-let secretNum = getRandomInt(20);
 let score = 20;
 let highScore = 0;
+let end = 10;
+document.querySelector('.end').value = 10;
+let secretNum = getRandomInt(1, 10);
+document.querySelector('.endNum-btn').addEventListener('click', () => {
+  end = document.querySelector('.end').value;
+  secretNum = getRandomInt(1, end);
+});
+
 function win() {
+  document.querySelector('.p-message').textContent = 'Correct Number!';
   document.querySelector('.logo-box').textContent = secretNum;
   if (highScore < score) {
     highScore = score;
@@ -15,40 +23,45 @@ function win() {
     'linear-gradient(rgba(50, 248, 24, 0.75), rgba(50, 248, 24, 1))';
   document.querySelector('.logo-box').style.width = '30rem';
 }
+const message = (selector, val) =>
+  (document.querySelector(selector).textContent = val);
+
 function eventHandlerCheck() {
+  gameLogic();
+}
+
+function gameLogic() {
   let guess = Number(document.querySelector('.input-guess').value);
-  if (!guess) {
-    document.querySelector('.p-message').textContent = 'Please enter a number!';
-  } else if (score === 0) {
-    document.querySelector('.p-message').textContent = 'You lose!';
-  } else if (guess === secretNum) {
-    score--;
-    document.querySelector('.score-count').textContent = score;
-    document.querySelector('.p-message').textContent = 'Correct Number!';
-    win();
-  } else if (guess < secretNum) {
-    document.querySelector('.p-message').textContent = 'Guess higher!';
-    score--;
-    document.querySelector('.score-count').textContent = score;
+  if (!guess || score === 1) {
+    message('.score-count', 0);
+    message('.p-message', !guess ? 'You must enter a guess!' : 'You loose!');
   } else {
-    document.querySelector('.p-message').textContent = 'Guess lower!';
     score--;
-    document.querySelector('.score-count').textContent = score;
+    message('.score-count', score);
+    if (guess === secretNum) win();
+    else message('.p-message', guess > secretNum ? 'Too high!' : 'Too low!');
   }
 }
 
 function eventHandlerPlayAgain() {
+  resetGame();
+}
+function resetGame() {
   score = 20;
-  secretNum = getRandomInt(20);
-  document.querySelector('.score-count').textContent = 20;
+  secretNum = getRandomInt(1, 10);
+  message('.score-count', 20);
+  message('.logo-box', '?');
+  message('.p-message', 'Good luck! 😎');
   document.querySelector('.input-guess').value = '';
-  document.querySelector('.logo-box').textContent = '?';
-  document.querySelector('.p-message').textContent = 'Good luck! 😎';
+  document.querySelector('.end').value = 10;
   document.querySelector('body').style.background =
     'linear-gradient(#555, #222)';
+  document.querySelector('.logo-box').style.width = '15rem';
 }
-document.querySelector('.check').addEventListener('click', eventHandlerCheck);
-
-document
-  .querySelector('.play-again')
-  .addEventListener('click', eventHandlerPlayAgain);
+function playGame() {
+  document.querySelector('.check').addEventListener('click', eventHandlerCheck);
+  document
+    .querySelector('.play-again')
+    .addEventListener('click', eventHandlerPlayAgain);
+}
+playGame();
